6b5cb2fcd380af1e612641a9e4f72dc5629a91cf
Afik Tahirov <afik.t38@gmail.com>
2023-09-05 00:22:59 +0400

    first commit

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json(
+            [
+                'results' => $users,
+            ],
+            200,
+        );
+    }
+
+    public function show($id)
+    {
+        // User details
+        $users = User::find($id);
+        if (!$users) {
+            return response()->json(
+                [
+                    'message' => 'User Not Found.',
+                ],
+                404,
+            );
+        }
+
+        // Return Json Response
+        return response()->json(
+            [
+                'users' => $users,
+            ],
+            200,
+        );
+    }
+}
